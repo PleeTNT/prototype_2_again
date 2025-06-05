d75afacf90431ec6ad2e81b241597702fe0f611e
Anderson_Preston <andep510@hsd.k12.or.us>
2025-06-05 10:37:34 -0700

    finished 2.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
 
+    public GameObject projectilePrefab;
+
     // Update is called once per frame
     void Update()
     {
@@ -24,5 +26,10 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange) {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (Input.GetKeyDown(KeyCode.Space)){
+            // launch projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
